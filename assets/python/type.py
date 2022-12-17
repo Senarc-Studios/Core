4af6340d7a64d6045ebbc9d40c3d580cb054a5f0
BenitzCoding <benitzcoding@yahoo.com>
2022-12-17 13:45:29 +0800

    Replaced Enum classes with static objects

--- assets/python/type.py
+++ assets/python/type.py
@@ -1,18 +1,16 @@
-from enum import Enum
-
 class Modmail:
-	class InteractionType(Enum):
+	class InteractionType:
 		DM = 1
 		THREAD = 2
 
-	class Action(Enum):
+	class Action:
 		CHECK_THREAD_EXISTANCE = 201
 		THREAD_DELETE = 202
 
-class ActionPacket(Enum):
+class ActionPacket:
 	CALLBACK = 1
 	HANDOFF = 2
 
-class CreateVoice(Enum):
+class CreateVoice:
 	CREATE_CHANNEL = 101
 
