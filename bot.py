c28aebe92b403f7d5a61a4a92e02bc7b4addac22
BenitzCoding <beni@senarc.net>
2023-07-14 22:26:36 +0800

    Fixed Application Command Sync.

--- bot.py
+++ bot.py
@@ -8,14 +8,16 @@ from discord.ext.commands import Bot
 from typing import Coroutine, Any
 
 class Senarc(Bot):
-	def __init__(self, *args, **kwargs):
+	def __init__(self, guild_id, *args, **kwargs):
+		self.guild_id = guild_id
 		super().__init__(*args, **kwargs)
 
 	async def start(self, *args, **kwargs):
 		super().start(*args, **kwargs)
 
 	async def sync_application(self):
-		await self.tree.sync(guild = Internal.core_guild)
+		await self.tree.sync(guild = self.guild_id)
+		await self.tree.sync()
 		print("Application synced successfully.")
 
 	async def setup_hook(self) -> Coroutine[Any, Any, None]:
@@ -25,19 +27,21 @@ class Senarc(Bot):
 
 		self.loop.create_task(self.sync_application())
 
-intents = Intents.all()
-bot = Bot(
-	command_prefix = "sca!",
-	intents = intents
-)
-
 fetch_list = (
 	"CLIENT_TOKEN",
+	"CORE_GUILD_ID"
 )
 internal = Internal()
 Constants = internal.Constants("./assets/json/constants.json")
 for constant in fetch_list:
 	Constants.fetch(constant)
+
+intents = Intents.all()
+bot = Senarc(
+	guild_id = int(Constants.get("CORE_GUILD_ID")),
+	command_prefix = "sca!",
+	intents = intents
+)
 
 if __name__ == "__main__":
 	bot.Constants = Constants
