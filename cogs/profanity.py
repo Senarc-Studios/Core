a80ca2001a0557f65a5d1916ad67f34247d328e4
BenitzCoding <beni@senarc.net>
2023-07-15 12:00:59 +0800

    Fixed Profanity Filters.

--- cogs/profanity.py
+++ cogs/profanity.py
@@ -3,7 +3,7 @@ from assets.python.internal import Internal
 from discord import Embed
 from discord.ext.commands import Cog
 
-from profanity_check import predict_prob
+from profanityfilter import ProfanityFilter
 
 internal = Internal()
 Constants = internal.Constants("./assets/json/constants.json")
@@ -16,14 +16,14 @@ for constant in FETCH_LIST:
 class Profanity(Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.filter = ProfanityFilter()
 
     @Cog.listener("on_message")
     async def profanity_check(self, message):
-        probability: int = int(predict_prob([message.content])[0] * 100)
-        print(probability)
-        if probability > 65:
+        is_profane: bool = self.filter.is_profane(message.content)
+        if is_profane:
             log_message = Embed(
-                description = f"**Original Message:**\n{message.content}\n\n**Probability:**\n`{probability}%`",
+                description = f"**Original Message:**\n{message.content}\n\n**Censored Message:**\n{self.filter.censor(message.content)}",
                 colour = 0x2B2D31
             )
             log_message.set_author(
